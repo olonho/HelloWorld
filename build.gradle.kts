0653c15cfd4e7f074e72ad33169c8ead442498f3
Nikolay Igotti <igotti@gmail.com>
2018-08-26 11:10:49 +0300

    Support mingw

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,13 +2,26 @@ plugins {
 	id("org.jetbrains.kotlin.konan").version("0.8.2")
 }
 
+val glfwMingw = System.getProperty("user.home") + "/HelloWorld/src/main/c_interop/glfw-3.2.1"
+val glfwMac = "/usr/local"
+
 konanArtifacts {
 	interop("glfw3") {
-           compilerOpts("-I/usr/local/include")
+           target("macos") {
+             compilerOpts("-I$glfwMac/include")
+           }
+           target("mingw") {
+             compilerOpts("-I$glfwMingw/include")
+           }
         }
 
 	program("HelloWorld") {
-                linkerOpts("-L/usr/local/lib")
+                target("macos") {
+                  linkerOpts("-L$glfwMac/lib")
+                }
+                target("mingw") {
+                  linkerOpts("-L$glfwMingw/lib-mingw-w64")
+                }
 		libraries {
 			artifact("glfw3")
 		}
